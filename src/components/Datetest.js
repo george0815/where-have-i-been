9fbdb5431f2079d45ad08d06abc6c4f187747a50
George S <shipmang8@gmail.com>
2024-03-07 18:53:29 -0500

    changed some stuff

--- src/components/Datetest.js
+++ src/components/Datetest.js
@@ -1,13 +1,19 @@
 
 import DatePicker from 'react-date-picker';
 import 'react-date-picker/dist/DatePicker.css';
+import { useState } from 'react';
+
 import 'react-calendar/dist/Calendar.css';
+
+
+
 export default function Datetest(props){
+    const [value, onChange] = useState(new Date());
 
 
     return (
 
- <DatePicker/>
+        <DatePicker onChange={onChange} value={value} />
     
     )
 
