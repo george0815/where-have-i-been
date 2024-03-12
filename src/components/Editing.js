c0a3bc95dc9962421266df2699708705e941900b
George S <shipmang8@gmail.com>
2024-03-12 08:35:53 -0400

    added foundations for displaying upload progress

--- src/components/Editing.js
+++ src/components/Editing.js
@@ -3,7 +3,7 @@ import { doc, setDoc } from "firebase/firestore";
 import {db} from "../firebase"
 import { storage} from "../firebase";
 import exifr from 'exifr' // => exifr/dist/full.umd.cjs
-import { uploadBytes, ref, getDownloadURL} from "firebase/storage";
+import { uploadBytesResumable, ref, getDownloadURL} from "firebase/storage";
 import DatePicker from 'react-date-picker';
 
 import {
@@ -127,14 +127,43 @@ export default function Editing(props) {
 
 
   function getURL(storageRef, file) {
-    return new Promise((resolve, reject) => uploadBytes(storageRef, file).then((snapshot) => {
+    return new Promise((resolve, reject) => {
+      
+      const uploadTask = uploadBytesResumable(storageRef, file);
 
-      getDownloadURL(snapshot.ref).then(function(downloadURL) {
-        resolve(downloadURL);
-      });          
-
-    }));
-  
+      // Register three observers:
+      // 1. 'state_changed' observer, called any time the state changes
+      // 2. Error observer, called on failure
+      // 3. Completion observer, called on successful completion
+      uploadTask.on('state_changed', 
+        (snapshot) => {
+          // Observe state change events such as progress, pause, and resume
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused');
+              break;
+            case 'running':
+              console.log('Upload is running');
+              break;
+          }
+        }, 
+        (error) => {
+          // Handle unsuccessful uploads
+        }, 
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log('File available at', downloadURL);
+            resolve(downloadURL);
+          });
+        }
+      );
+    
+    })
   }
 
   async function addPhotoAlbumData(src){
