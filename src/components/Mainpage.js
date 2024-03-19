d2f2c41e4a82fcd21943a64638c68e8cfba72215
George S <shipmang8@gmail.com>
2024-03-19 15:17:52 -0400

    saving progress

--- src/components/Mainpage.js
+++ src/components/Mainpage.js
@@ -1,6 +1,7 @@
 import { useState, React } from 'react'; //React
 import Background from './Background'; //Gets background
 import Navbar from './Navbar'; //navbar
+import Editing from './Editing'; //editing component
 //CSS
 import '../styles/Mainpage.css';
 import '../styles/Fonts.css';
@@ -15,6 +16,36 @@ export default function Mainpage(props) {
   //sets captions
   function setCaption(cap) { setCurrentCaption(cap);}
 
+
+  //sets up state for editing props
+  const [editingSettings, setEditingSettings] = useState(
+
+    {
+        adding: false,
+        isAlbum: true,
+        saveButtonText: "Save",
+        currentlyEditing: false
+    }
+  )
+
+
+
+
+  //creates guest album
+  function onClickCreateAlbumAsGuest(){ setEditingSettings(
+    
+    //ask user via alert
+
+
+    {
+        adding: true,
+        isAlbum: true,
+        saveButtonText: "Add",
+        currentlyEditing: true
+    }
+
+  );}
+
   return (
 
     //holds entire page
@@ -25,12 +56,17 @@ export default function Mainpage(props) {
       {/*Holds all elements in the main page*/}
       <div className="mainPageContainer">
 
+        {/*Contains fullscreen version of photo*/}
+        <div className={editingSettings.currentlyEditing ? 'editingComponentContainer activeEditing' : 'editingComponentContainer'}>  
+            <Editing  loggedIn={props.loggedIn} editingSettings={editingSettings} onEditExit={onEditExit} currentPhoto={currentPhoto} currentAlbum={currentAlbum}/>
+        </div>
+
         {/*Holds all the text in the main page, except for picture caption at the bottom left*/}
         <div className="mainTextContainer">A free, online photo album
             <br/>
                 <span>Never lose your memories, <br/>save every experience</span>
             <br/><br/><br/><br/><br/><br/>
-            <p>Login or <button>create an album as a guest</button></p>
+            <p>Login or <button onClick={onClickCreateAlbumAsGuest}>create an album as a guest</button></p>
         </div>
         
         
